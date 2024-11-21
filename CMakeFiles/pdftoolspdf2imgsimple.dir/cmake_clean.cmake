233f6c0137faefdb7ad56281c5bdcc287a1bebd2
ManuelNevado <nevadofabianmanuel@gmail.com>
2024-11-21 13:35:47 +0100

    feat: create commit

---
+++ CMakeFiles/pdftoolspdf2imgsimple.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pdftoolspdf2imgsimple.dir/pdftoolspdf2imgsimple.c.o"
+  "CMakeFiles/pdftoolspdf2imgsimple.dir/pdftoolspdf2imgsimple.c.o.d"
+  "pdftoolspdf2imgsimple"
+  "pdftoolspdf2imgsimple.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pdftoolspdf2imgsimple.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/pdftoolspdf2imgsimple.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for pdftoolspdf2imgsimple.
+# This may be replaced when dependencies are built.
